3f62ad3b993a95f46861ecd687444823431dcb46
chivicoko <victor.c.okoye@gmail.com>
2024-07-14 09:16:51 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Uncomment this if you need a base path
   // base: "/sweettooth/",
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
         target: 'https://timbu-get-all-products.reavdev.workers.dev/',
-        // target: 'https://api.timbu.cloud',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
