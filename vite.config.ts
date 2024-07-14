304b5343b00a500e4f32b80e801427c81cd0ffc0
chivicoko <victor.c.okoye@gmail.com>
2024-07-14 11:39:18 +0100

    update api endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,36 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
+
+// export default defineConfig({
+//   base: "/",
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: baseURL,
+//         // target: 'https://api.timbu.cloud',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const baseURL = process.env.VITE_APP_API_BASE_URL;
+
+if (!baseURL) {
+  throw new Error('VITE_APP_API_BASE_URL environment variable is not defined');
+}
 
 export default defineConfig({
   base: "/",
@@ -7,8 +38,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://timbu-get-all-products.reavdev.workers.dev/',
-        // target: 'https://api.timbu.cloud',
+        target: baseURL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
