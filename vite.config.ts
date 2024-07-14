4b3272afdd33c300ef4d4e58da354e3438414bb2
chivicoko <victor.c.okoye@gmail.com>
2024-07-14 08:06:13 +0100

    update...

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,42 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   // base: "/sweettooth/",
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://timbu-get-all-products.reavdev.workers.dev/',
-//         // target: 'https://api.timbu.cloud',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, '')
-//       }
-//     }
-//   }
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // base: "/sweettooth/",
   plugins: [react()],
   server: {
-        proxy: {
-          '/api': {
-            target: 'https://timbu-get-all-products.reavdev.workers.dev/',
-            // target: 'https://api.timbu.cloud',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, '')
-          }
-        }
-      },
-  build: {
-    outDir: 'dist'
-  },
-  // server: {
-  //   open: true,
-  //   port: 3000
-  // }
+    proxy: {
+      '/api': {
+        target: 'https://timbu-get-all-products.reavdev.workers.dev/',
+        // target: 'https://api.timbu.cloud',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//         proxy: {
+//           '/api': {
+//             target: 'https://timbu-get-all-products.reavdev.workers.dev/',
+//             // target: 'https://api.timbu.cloud',
+//             changeOrigin: true,
+//             rewrite: (path) => path.replace(/^\/api/, '')
+//           }
+//         }
+//       },
+//   build: {
+//     outDir: 'dist'
+//   },
+//   // server: {
+//   //   open: true,
+//   //   port: 3000
+//   // }
+// });
